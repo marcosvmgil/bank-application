e2487b6e54cc73425c4a93c4bdfd0b28c3077f74
marcosvmgil <viniciusmarcos_v@hotmail.com>
2024-08-28 10:54:36 -0300

    Styling navbar and creating funcions

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private router: Router) {}
+  protected routes = [
+    {
+      path: '',
+      page: 'Cadastro',
+    },
+    {
+      path: 'transfer',
+      page: 'Transferencias',
+    },
+
+    {
+      path: 'deposit',
+      page: 'Depósito',
+    },
+
+    {
+      path: 'extract',
+      page: 'Consulta Extrato',
+    },
+  ];
+
+  navToRoute(route: string) {
+    this.router.navigate([route]);
+  }
+
+  currentRoute(path: string) {
+    return this.router.url == `/${path}`;
+  }
+}
